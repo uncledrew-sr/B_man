2dd9191f7cce5038fc7179f9fddad52498e30a1c
uncledrew-sr <bonggutravel@gmail.com>
2025-03-14 23:56:30 +0900

    main dart done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'src/app.dart'; // app.dart의 클래스를 가져온다.
 
 void main(){
   runApp(const MyApp());
@@ -9,12 +11,22 @@ class MyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Image.asset('assets/images/logo_simbol.png'),
+    return GetMaterialApp(
+      title: "Carrot clone coding",
+      initialRoute: '/',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          color: Color(0xff212123),
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+          ),
         ),
-      )
+        scaffoldBackgroundColor: const Color(0xff212123)
+      ),
+      getPages: [
+        GetPage(name: '/', page: () => const App()),
+      ],
     );
   }
 }
